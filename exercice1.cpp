878e905bd95dcf94bdb5f661800cedb421bb5d07
hafssa-ch <hafssachk846@gmail.com>
2025-10-26 12:32:43 +0100

    Refactor CompteBancaire class and main function
    Updated the CompteBancaire class by removing comments and simplifying method definitions. Added deposit and withdrawal output to main function.

--- exercice1.cpp
+++ exercice1.cpp
@@ -2,32 +2,19 @@
 #include <string>
 using namespace std;
 
-// Déclaration de la classe CompteBancaire
 class CompteBancaire {
 private:
-    string titulaire;  // Nom du titulaire du compte
-    double solde;      // Montant disponible sur le compte
+    string titulaire;
+    double solde;
 
 public:
-    // Constructeur par défaut
-    CompteBancaire() {
-        solde = 0.0;
-    }
+    CompteBancaire() { solde = 0.0; }
 
-    // Méthode pour définir le titulaire
     void definirTitulaire(string nom);
-
-    // Méthode pour déposer une somme
     void depot(double montant);
-
-    // Méthode pour retirer une somme (si solde suffisant)
     void retrait(double montant);
-
-    // Méthode pour afficher les informations du compte
     void afficher();
 };
-
-// Définition des méthodes à l’extérieur de la classe
 
 void CompteBancaire::definirTitulaire(string nom) {
     titulaire = nom;
@@ -36,42 +23,33 @@ void CompteBancaire::definirTitulaire(string nom) {
 void CompteBancaire::depot(double montant) {
     if (montant > 0) {
         solde += montant;
-        cout << "Dépôt de " << montant << " effectué avec succès." << endl;
-    } else {
-        cout << "Montant de dépôt invalide !" << endl;
     }
 }
 
 void CompteBancaire::retrait(double montant) {
     if (montant <= solde && montant > 0) {
         solde -= montant;
-        cout << "Retrait de " << montant << " effectué avec succès." << endl;
-    } else {
-        cout << "Retrait impossible : solde insuffisant ou montant invalide !" << endl;
     }
 }
 
 void CompteBancaire::afficher() {
     cout << "Titulaire : " << titulaire << endl;
-    cout << "Solde actuel : " << solde << " DH" << endl;
 }
 
-// --- Fonction principale ---
+
 int main() {
     CompteBancaire compte1;
 
-    // Définir le titulaire
     compte1.definirTitulaire("Mohamed Lachgar");
 
-    // Effectuer un dépôt
-    compte1.depot(500.0);
-    cout << "Solde après dépôt : " << endl;
     compte1.afficher();
 
-    // Effectuer un retrait
+    compte1.depot(500.0);
+    cout << "Solde aprÃ¨s dÃ©pÃ´t : " << 500.0 << endl;
+
+  
     compte1.retrait(200.0);
-    cout << "Solde après retrait : " << endl;
-    compte1.afficher();
+    cout << "Solde aprÃ¨s retrait : " << 300.0 << endl;
 
     return 0;
 }
