5a6094d3d1145f75dce989db3e884ead0a141e1a
hafssa-ch <hafssachk846@gmail.com>
2025-10-26 12:33:37 +0100

    Refactor Livre class to use 'string' directly

--- exercice2.cpp
+++ exercice2.cpp
@@ -1,38 +1,37 @@
 #include <iostream>
 #include <string>
+using namespace std;
 
 class Livre {
 private:
-    std::string titre;
-    std::string auteur;
+    string titre;
+    string auteur;
     int anneePublication;
 
 public:
-    // Déclaration des méthodes
-    void definirLivre(std::string t, std::string a, int annee);
+    
+    void definirLivre(string t, string a, int annee);
     void afficher() const;
     int getAnneePublication() const;
 };
 
-// Définition des méthodes en dehors de la classe
 
-void Livre::definirLivre(std::string t, std::string a, int annee) {
+void Livre::definirLivre(string t, string a, int annee) {
     titre = t;
     auteur = a;
     anneePublication = annee;
 }
 
 void Livre::afficher() const {
-    std::cout << "Titre : " << titre 
-              << ", Auteur : " << auteur 
-              << ", Année : " << anneePublication << std::endl;
+    cout << "Titre : " << titre
+         << ", Auteur : " << auteur
+         << ", Année : " << anneePublication << endl;
 }
 
 int Livre::getAnneePublication() const {
     return anneePublication;
 }
 
-// Fonction principale
 int main() {
     Livre livre1, livre2;
 
